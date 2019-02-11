2367d0a2552190708460d5ef20c4e6c075b2efe5
tslight <tobyslight@gmail.com>
2019-02-11 22:09:17 +0000

    mkfile still serves a purpose... I think ... !

--- chkpath/make.py
+++ chkpath/make.py
@@ -6,14 +6,37 @@ import os
 
 
 def mkdir(path):
+    """
+    Make nested directories if they don't exist. Pretty redundant since
+    os.makedirs does this anyway, but I've come this far, so..
+
+    Quite nice to have as a command line util though... except mkdir -p
+    exists... Goddamnit!
+    """
     if os.path.exists(path):
         print("{} already exists.".format(path))
     else:
-        os.makedirs(path)
+        try:
+            os.makedirs(path, exist_ok=True)
+        except OSError:
+            print("Uh oh - something went awry!")
+        else:
+            print("Successfully created {}".format(path))
 
 
 def mkfile(path):
+    """
+    Makes files in nested directories. I think this one is actually mildly
+    useful cos it creates the parent directory if that doesn't exist.
+    """
     if os.path.exists(path):
         print("{} already exists.".format(path))
     else:
-        open(path, 'a').close()
+        try:
+            parent = os.path.abspath(os.path.join(path, os.pardir))
+            os.makedirs(parent, exist_ok=True)
+            open(path, 'a').close()
+        except OSError:
+            print("Uh oh - something went awry!")
+        else:
+            print("Successfully created {}".format(path))
